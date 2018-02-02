2f5c050cfacdc567eac8aedfa801b87c786ed7e6
Alvin Go <alvingogo25@gmail.com>
2018-02-02 00:11:14 -0800

    successful spotify response

--- liri.js
+++ liri.js
@@ -1,22 +1,54 @@
 require("dotenv").config();
-var keys = require('/.keys.js');
 
-if (process.argv[2] === 'my-tweets') {
-	console.log('show tweets');
-};
+var keys = require('./keys.js');
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var request = require("request");
 
-if (process.argv[2] === 'spotify-this-song') {
-	console.log('name this song');
-};
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
-if (process.argv[2] === 'movie-this') {
-	if (process.argv[3]) {
-		console.log(process.argv[3])
-	} else {
-		console.log('Mr. Nobody');
+var user_method = process.argv[2];
+var user_input = process.argv[3];
+
+function tweets() {
+	if (user_method === 'my-tweets') {
+		console.log('show tweets');
 	}
 };
 
-if (process.argv[2] === 'do-what-it-says') {
-	console.log('calls command in random.txt');
-}
+
+function music() {
+	if (user_method === 'spotify-this-song') {
+		spotify.search({
+			type: 'track',
+			query: user_input
+		}, function(err, data) {
+			if (err) {
+				console.log('Error occurred: ' + err);
+				return;
+			} else {
+				// console.log(JSON.stringify(data, null, 2));
+				console.log(data.tracks.items[0].album.name);
+			}
+		})
+	}
+};
+music ();
+
+
+function movie() {
+	if (user_method === 'movie-this') {
+		if (process.argv[3]) {
+			console.log(process.argv[3])
+		} else {
+			console.log('Mr. Nobody');
+		}
+	};
+};
+
+function random() {
+	if (user_method === 'do-what-it-says') {
+		console.log('calls command in random.txt');
+	}
+};
